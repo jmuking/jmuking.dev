6d6a4f708224d2cbe6ad2c84e00cfeef1b53d246
Jeffrey King <jmuking@gmail.com>
2022-03-15 10:45:15 -0500

    wrapping text

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,17 +10,20 @@ function Home() {
       }}
     >
       <h2 style={{ marginTop: 0 }}>Sr. Software Engineer</h2>
-      <div style={{ display: "flex" }}>
+      <div>
         <img
           src={meJpg}
           alt="me"
           width="100%"
           style={{
             display: "block",
-            maxWidth: "min(50%, 671px)",
-            maxHeight: "min(35%, 604px)",
+            maxWidth: "min(65%, 671px)",
+            maxHeight: "min(46%, 604px)",
             width: "auto",
             height: "auto",
+            float: "left",
+            marginRight: "2rem",
+            marginBottom: "2rem",
           }}
         ></img>
         <p
@@ -28,7 +31,6 @@ function Home() {
             lineHeight: "1.8",
             marginTop: 0,
             marginBottom: 0,
-            marginLeft: "2rem",
           }}
         >
           I am a full stack software engineer who works mainly with React + TS
