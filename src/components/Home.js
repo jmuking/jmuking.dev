ac591a21cd6ef83712742d356a2aafca97769868
Jeffrey King <jmuking@gmail.com>
2022-03-15 10:30:37 -0500

    re-organize home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,18 +9,27 @@ function Home() {
         alignItems: "center",
       }}
     >
-      <h2>Sr. Software Engineer</h2>
-      <img
-        src={meJpg}
-        alt="me"
-        width="100%"
-        style={{ maxWidth: "min(50%, 500px)" }}
-      ></img>
-      <p style={{ lineHeight: "1.8" }}>
-        I am a full stack software engineer who works mainly with React + TS on
-        the front end, and Python on the back end. I am currently working as a
-        Sr. Software Engineer at Esri.
-      </p>
+      <h2 style={{ marginTop: 0 }}>Sr. Software Engineer</h2>
+      <div style={{ display: "flex" }}>
+        <img
+          src={meJpg}
+          alt="me"
+          width="100%"
+          style={{ maxWidth: "min(50%, 500px)" }}
+        ></img>
+        <p
+          style={{
+            lineHeight: "1.8",
+            marginTop: 0,
+            marginBottom: 0,
+            marginLeft: "2rem",
+          }}
+        >
+          I am a full stack software engineer who works mainly with React + TS
+          on the front end, and Python on the back end. I am currently working
+          as a Sr. Software Engineer at Esri.
+        </p>
+      </div>
     </div>
   );
 }
