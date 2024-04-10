4fff12c193324d68a75c589640339fb704991644
Jeffrey King <jeff@jmuking.dev>
2024-04-10 12:30:23 -0500

    Update Chill.js

--- src/components/Chill.js
+++ src/components/Chill.js
@@ -3,7 +3,7 @@ import Frame from "./Frame/Frame";
 function Chill() {
   return (
     <Frame
-      src="https://open.spotify.com/embed/playlist/37i9dQZF1E8QCgyqwrjY7z?utm_source=generator&theme=0"
+      src="https://open.spotify.com/embed/album/5mvHTVy1iUIkAflIufoOpj?utm_source=generator&theme=0"
       title="Chill Tunes"
       fullHeight={true}
     ></Frame>
